483cfb7e4fdb16756c12641c9e3dcd01e9f03bcc
deybacsi <deyprof@gmail.com>
2019-09-03 22:23:19 +0200

    a

--- updater.sh
+++ updater.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 clockpid=$(ps -ef | grep asciiclock | grep -vE '(grep|g++)' | awk '{print }')
 
+myfetch=$(git fetch --dry-run)
+
+if [ "$myfetch" -eq "" ]; then
+	echo "No updates, exiting"
+else
+	echo "Update found"
+fi
 

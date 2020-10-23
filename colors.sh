7679324edde7901d67d91ae1d93bdfe961e26494
dey <deyprof@gmail.com>
2020-10-23 16:54:15 +0200

    added color config

---
+++ colors.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -en "\e]P0000000"
+echo -en "\e]P1AA0000"
+echo -en "\e]P244AA44"
+echo -en "\e]P3AA5500"
+echo -en "\e]P40039AA"
+echo -en "\e]P5AA22AA"
+echo -en "\e]P61A92AA"
+echo -en "\e]P7AAAAAA"
+echo -en "\e]P8777777"
+echo -en "\e]P9FF8787"
+echo -en "\e]PA4CE64C"
+echo -en "\e]PBDED82C"
+echo -en "\e]PC295FCC"
+echo -en "\e]PDCC58CC"
+echo -en "\e]PE4CCCE6"
+echo -en "\e]PFFFFFFF"
+
